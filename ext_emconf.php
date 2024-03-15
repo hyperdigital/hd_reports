9dc96e08c17a17353abc33bc60fdbaf0b6e18b4e
Martin Pribyl <martin.pribyl@hyperdigital.de>
2024-03-15 08:53:57 +0100

    updated version names

--- ext_emconf.php
+++ ext_emconf.php
@@ -17,10 +17,10 @@ $EM_CONF[$_EXTKEY] = array(
     'modify_tables' => '',
     'clearCacheOnLoad' => 0,
     'lockType' => '',
-    'version' => '11.0.0',
+    'version' => '1.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '11.0.0-11.99.99',
+            'typo3' => '11.0.0-12.99.99',
         ],
         'conflicts' => [],
         'suggests' => [],
